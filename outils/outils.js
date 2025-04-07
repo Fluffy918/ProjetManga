a7683b165b40e94e6b0f7e20de97c2f70ee63562
Brendan <pidouxbrendan@gmail.com>
2025-04-07 15:02:49 +0200

    outils

--- outils/outils.js
+++ outils/outils.js
@@ -1,7 +1,28 @@
 // Fonction inséré un chapitre
+import mysql from 'mysql2/promise'
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password: '',
+    database: 'manga_db'
+})
+
+const mangaId = ''
+
+const chapitres = []
+
+for (let i = 1; i <= 7; i++) {
+    chapitres.push({
+        numero: i,
+        titre: `Chapitre ${i}`,
+        date_sortie: '',
+        url: ''
+    })
+}
+
 async function insertChapitres() {
     for (const chapitre of chapitres) {
-        await connextion.execute(
+        await connection.execute(
             "INSERT INTO chapitres (manga_id, numero, titre, date_sortie, url) VALUES(?, ?, ?, ?, ?)",
             [mangaId, chapitre.numero, chapitre.titre, chapitre.date_sortie, chapitre.url]
         )
